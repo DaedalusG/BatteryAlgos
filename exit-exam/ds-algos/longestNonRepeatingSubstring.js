6f11188e70dabff1d085a97ea425db27ff07858b
warrgiff <warrenbruceg@gmail.com>
2020-11-17 15:28:02 -0800

    solved longestNonRepeatingSubstring

--- exit-exam/ds-algos/longestNonRepeatingSubstring.js
+++ exit-exam/ds-algos/longestNonRepeatingSubstring.js
@@ -1,15 +1,20 @@
 const longestNonRepeatingSubstring = function (string) {
-    let result = 0
+    let length = 0
     let currStr = []
 
     for (let i = 0; i < string.length; i++) {
-        if (currStr.indexOf(string[i])) {
+        if (!string.length) return 0
+
+        if (currStr.indexOf(string[i]) !== -1) {
             currStr = [string[i]]
         } else {
             currStr.push(string[i])
+            if (currStr.length > length) {
+                length = currStr.length;
+            }
         }
-
     }
+    return length
 };
 
 module.exports = longestNonRepeatingSubstring;
