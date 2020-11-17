cf4f363c1d4e8ca9a880f4000eb9e8829e1b7d3c
warrgiff <warrenbruceg@gmail.com>
2020-11-17 14:58:50 -0800

    working on the first algorithm

--- exit-exam/ds-algos/longestNonRepeatingSubstring.js
+++ exit-exam/ds-algos/longestNonRepeatingSubstring.js
@@ -1,5 +1,15 @@
 const longestNonRepeatingSubstring = function (string) {
+    let result = 0
+    let currStr = []
 
+    for (let i = 0; i < string.length; i++) {
+        if (currStr.indexOf(string[i])) {
+            currStr = [string[i]]
+        } else {
+            currStr.push(string[i])
+        }
+
+    }
 };
 
 module.exports = longestNonRepeatingSubstring;
